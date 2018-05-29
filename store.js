6461580cdd92b8c2873178b65bc7c1514c5967a9
Addison Schultz <addisonschultz0@gmail.com>
2018-05-29 14:38:33 +0200

    adding to shopping cart working

--- store.js
+++ store.js
@@ -1,4 +1,5 @@
 // TODO:
+// Declare `products`, the different that you will be selling under each of the departments.
 // var products = []
 var products = [
   { referenceNumber: 1231, name: "Super Lite Mat", price: 10 },
@@ -12,12 +13,9 @@ var products = [
 ];
 // Declare `shoppingCart`, something where you will be storing all products that the user buys.
 var shoppingCart = [];
-// Declare `products`, the different that you will be selling under each of the departments.
 
-
-var shopFromStore =  function () {
+var shopFromStore =  function() {
   var refNr = askUserForReferenceNumber();
-  // Add the product with the matching referenceNumber to the shoppingCart
 
   displayProductsFromShoppingCart();
 
@@ -29,16 +27,63 @@ var shopFromStore =  function () {
   // if no, print the goodbye message
 };
 
+
 var displayProductsFromShoppingCart = function() {
   // iterate over the shoppingCart and display the contents
 
+    products.forEach( function(product) {
+      if (product.referenceNumber == refNr) {
+        var x = product;
+        // console.log(x);
+        shoppingCart.push(product);
+      }
+    });
   // use the printProductsOnScreen function for inspiration
+
+  for(var item of shoppingCart){
+
+    // create elements for refNr, name, price, with a class and the proper innerText
+    var referenceNumberElement2 = document.createElement('span');
+    referenceNumberElement2.className  = 'referenceNumber';
+    referenceNumberElement2.innerText = item.referenceNumber;
+
+    var nameElement2 = document.createElement('span');
+    nameElement2.className  = 'name';
+    nameElement2.innerText = item.name;
+
+    var priceElement2 = document.createElement('span');
+    priceElement2.className  = 'price';
+    priceElement2.innerText = item.price;
+
+    // Wrap our just created elements in a div
+    var productElement2 = document.createElement('div');
+    productElement2.className = 'shopingcart';
+
+    productElement2.appendChild(referenceNumberElement2);
+    productElement2.appendChild(nameElement2);
+    productElement2.appendChild(priceElement2);
+
+    // Hang that div on the page
+    document.getElementById('shopping-cart').appendChild(productElement2);
+
+
+  }
 };
 
 var askUserForReferenceNumber = function() {
   // Use window.prompt to ask the user a question and capture their response,
   // then, return the response from this function back to our caller
+  refNr = window.prompt("What Product would you like to add to the cart? Please type in the corresponding Reference Number.");
 };
+
+
+
+
+
+
+
+
+
 
 //
 // do not change the code below (but feel free to change it if your WHOLE project works!)
@@ -47,6 +92,8 @@ var askUserForReferenceNumber = function() {
 var displayTotalPrice = function (amount = 0) {
   document.getElementById('total-price').innerText = amount;
 };
+
+
 
 var printProductsOnScreen = function () {
   for(var product of products){
